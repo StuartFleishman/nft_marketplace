224a2a35730d7d691bed24c5aef04cadccf8656c
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-11-27 20:09:01 -0500

    added to nav bar

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,26 @@ import '../styles/globals.css'
 import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+    <nav className="border-b p-6">
+      <p className="text-4xl font-bold">Stu's NFT place</p>
+      <div className="flex mt-4">
+        <Link href="/">
+          <a className="mr-4 text-blue-500">
+            Home
+          </a>
+        </Link>
+        <Link href="/create-item">
+            <a className="mr-6 text-pink-500">
+              Sell Digital Asset
+            </a>
+        </Link>
+      </div>
+    </nav>
+     <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
