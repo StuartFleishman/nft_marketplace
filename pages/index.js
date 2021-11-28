40dfb9d2b0cba92ff4bffda580b0927b5380d762
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-11-27 20:23:22 -0500

    importing nft and market from artifacts

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,9 @@ import {
   nftaddress, nftmarketaddress
 } from '../config'
 
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+import Market from '../artifacts/contracts/Market.sol/NFTMarket.json'
+
 
 
 export default function Home() {
