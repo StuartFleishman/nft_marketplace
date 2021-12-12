7251905a206ac698672397911e1f946a21d3267b
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-12-12 15:42:47 -0500

    setting up unit256 and string for name

--- Storage.sol
+++ Storage.sol
@@ -4,5 +4,13 @@ pragma solidity >=0.6.0 <0.9.0;
 
 contract SimpleStorage {
 
+        uint256 favoriteNumber;
+
+    // This is a comment!
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
     
 }
